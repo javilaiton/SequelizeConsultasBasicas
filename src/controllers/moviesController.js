52b0b4d768f051476d4063dd0fd223c9bf3b717d
javilaiton <laiton2395@gmail.com>
2021-12-12 01:23:47 -0500

    se agrega el metodo recomended, funciona sin el where

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -24,8 +24,20 @@ const moviesController={
             ]
         })
             .then(function(movies){
-                res.render("moviesList",{movies:movies})
+                res.render("newestMovies",{movies:movies})
             })
+    },
+    recomended:(req,res)=>{
+        db.movies.findAll({
+            order:[
+                ["release_date","DESC"]
+            ],
+            limit:5
+        })
+            .then(function(movies){
+                res.render("recommendedMovies",{movies:movies})
+            })
+
     }
 }
 
